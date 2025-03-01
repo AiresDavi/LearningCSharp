90f797155ed606566604e8db6d3bcd017910bb74
daviAires <airesdavi@gmail.com>
2025-02-28 23:43:35 -0300

    Primeiro commit

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -8,25 +8,8 @@ namespace LearningC_
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-
-            Console.Write("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\nHello, " + name + "! Your age is " + age + ".");
-
-            if (age < 18)
-            {
-                Console.WriteLine("\nSorry, you cannot enter.");
-            }
-            else
-            {
-                Console.WriteLine("\nYou are weolcome!");
-            }
-
+        static void Main(string[] args){
+            Console.WriteLine("Hello World");
         }
     }
 }
